9db9a613af486944170c96dc570e7939c868edf9
KFolwarski20 <konfolw@wp.pl>
2026-01-21 00:06:42 +0100

    Implement Contact model to connect followers by ManyToMany relationship

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
