a053578d67d3f07dca032dc9f6e32bb8f25c7dad
KFolwarski20 <konfolw@wp.pl>
2026-01-25 00:46:43 +0100

    Define configuration class for app

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, default='img/domyslny_avatar.jpg')
 
     def __str__(self):
         return f'Profil użytkownika {self.user.username}'
--- bookmarks/images/apps.py
+++ bookmarks/images/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+
+    def ready(self):
+        # Import procedur obsługi sygnałów
+        import images.signals
